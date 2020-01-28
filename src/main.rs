1cd66476dbf7cc7a0e7f17add02ca6aeb5114ec5
Emmanuel Touzery <etouzery@gmail.com>
2020-01-28 20:38:06 +0100

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use gdk;
 use gtk::prelude::*;
 use gtk::{ImageExt, Inhibit};
 use relm::{ContainerWidget, Widget};
 use relm_derive::{widget, Msg};
-use EventListItemMsg::EventSelected as EventListItemEventSelected;
 
 const FONT_AWESOME_SVGS_ROOT: &str = "fontawesome-free-5.12.0-desktop/svgs/solid";
 
@@ -28,10 +26,7 @@ impl Widget for Win {
     fn init_view(&mut self) {
         self.event_pane.set_size_request(350, -1);
         for event in &self.model.events {
-            let child = self.event_list.add_widget::<EventListItem>(event.clone());
-
-            relm::connect!(child@EventListItemEventSelected(ref event),
-                           self.model.relm, Msg::EventSelected(event.clone()));
+            let _child = self.event_list.add_widget::<EventListItem>(event.clone());
         }
 
         relm::connect!(
@@ -148,46 +143,22 @@ impl Widget for Win {
 /// EventListItem
 
 #[derive(Msg)]
-pub enum EventListItemMsg {
-    Click,
-    EventSelected(Event),
-}
+pub enum EventListItemMsg {}
 
 pub struct EventListItemModel {
-    relm: relm::Relm<EventListItem>,
     event: Event,
 }
 
 #[widget]
 impl Widget for EventListItem {
-    fn model(relm: &relm::Relm<Self>, event: Event) -> EventListItemModel {
-        EventListItemModel {
-            relm: relm.clone(),
-            event,
-        }
+    fn model(event: Event) -> EventListItemModel {
+        EventListItemModel { event }
     }
 
-    fn update(&mut self, event: EventListItemMsg) {
-        match event {
-            EventListItemMsg::Click => self
-                .model
-                .relm
-                .stream()
-                .emit(EventListItemMsg::EventSelected(self.model.event.clone())),
-            _ => (),
-        }
-    }
+    fn update(&mut self, _event: EventListItemMsg) {}
 
     view! {
         gtk::EventBox {
-            button_press_event(_, event) => ({
-                if event.get_event_type() == gdk::EventType::DoubleButtonPress {
-                    EventListItemMsg::Click
-                }
-                else {
-                    EventListItemMsg::Click
-                }
-            }, Inhibit(false)),
             gtk::Box {
                 orientation: gtk::Orientation::Horizontal,
                 margin_start: 10,
@@ -385,64 +356,3 @@ fn fontawesome_image(image_name: &str) -> gdk_pixbuf::Pixbuf {
 fn main() {
     Win::run(()).unwrap();
 }
-
-// fn single_event<F: 'static + Fn(&Rc<Event>)>(event: &Rc<Event>, clicked: Rc<F>) -> gtk::EventBox {
-//     let hbox = Box::new(gtk::Orientation::Horizontal, 10);
-
-//     let vbox_eventtype = Box::new(gtk::Orientation::Vertical, 2);
-//     vbox_eventtype.add(&fontawesome_image(event.event_type.get_icon()));
-//     vbox_eventtype.add(&Label::new(Some(event.event_type.get_desc())));
-//     hbox.add(&vbox_eventtype);
-
-//     let vbox_eventdetails = Box::new(gtk::Orientation::Vertical, 2);
-//     let vbox_eventtime_and_extra_info = Box::new(gtk::Orientation::Horizontal, 2);
-//     let time_label = Label::new(Some(&format!("<b>{}</b>", event.event_time)));
-//     time_label.set_use_markup(true);
-//     time_label.set_halign(gtk::Align::Start);
-//     vbox_eventtime_and_extra_info.pack_start(&time_label, true, true, 0);
-//     let extra_details_label = Label::new(event.event_extra_details.as_ref().map(|t| t.as_str()));
-//     extra_details_label.set_halign(gtk::Align::Start);
-//     vbox_eventtime_and_extra_info.pack_end(&extra_details_label, false, false, 0);
-//     vbox_eventdetails.pack_start(&vbox_eventtime_and_extra_info, true, true, 5);
-//     let details_label = Label::new(Some(&event.event_info));
-//     details_label.set_halign(gtk::Align::Start);
-//     vbox_eventdetails.pack_start(&details_label, true, false, 5);
-//     hbox.pack_start(&vbox_eventdetails, true, true, 5);
-
-//     hbox.set_margin_start(10);
-//     hbox.set_margin_end(10);
-//     hbox.set_margin_top(10);
-//     hbox.set_margin_bottom(10);
-
-//     let event_box = gtk::EventBox::new();
-//     event_box.add(&hbox);
-
-//     event_box
-//         .connect_local(
-//             // i don't understand the diff between connect and connect_local!!
-//             // but connect wants some multithread things that connect_local doesn't require
-//             "button-press-event",
-//             false,
-//             glib::clone!(@strong event => move |_| {
-//                 clicked(&event);
-//                 Some(true.to_value())
-//             }),
-//         )
-//         .unwrap();
-
-//     event_box
-// }
-
-// fn event_list<F: 'static + Fn(&Rc<Event>)>(
-//     events: &Vec<Rc<Event>>,
-//     event_selected: Rc<F>,
-// ) -> gtk::ScrolledWindow {
-//     let vbox = Box::new(gtk::Orientation::Vertical, 0);
-//     for event in events {
-//         vbox.add(&single_event(event, event_selected.clone()));
-//     }
-//     let scrolled_win = ScrolledWindow::new(None::<&gtk::Adjustment>, None::<&gtk::Adjustment>);
-//     scrolled_win.add(&vbox);
-//     scrolled_win.set_size_request(350, -1);
-//     scrolled_win
-// }
