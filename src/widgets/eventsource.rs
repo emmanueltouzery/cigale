421ed51734c7a3aa48049065597275e7ecb176f9
Emmanuel Touzery <etouzery@gmail.com>
2020-02-13 22:16:33 +0100

    nicer look for the list of event sources

--- src/widgets/eventsource.rs
+++ src/widgets/eventsource.rs
@@ -15,11 +15,24 @@ pub struct EventSourceListItemModel {
 #[widget]
 impl Widget for EventSourceListItem {
     fn init_view(&mut self) {
+        let mut i = 1;
         for kv in &self.model.event_source {
-            let hbox = gtk::Box::new(gtk::Orientation::Horizontal, 5);
-            hbox.add(&gtk::Label::new(Some(kv.0)));
-            hbox.add(&gtk::Label::new(Some(kv.1)));
-            self.items_box.add(&hbox);
+            let desc = gtk::LabelBuilder::new().label(kv.0).xalign(0.0).build();
+            desc.get_style_context()
+                .add_class("event_source_config_label");
+            self.items_box.attach(&desc, 0, i, 1, 1);
+            self.items_box.attach(
+                &gtk::LabelBuilder::new()
+                    .label(kv.1)
+                    .ellipsize(pango::EllipsizeMode::End)
+                    .xalign(0.0)
+                    .build(),
+                1,
+                i,
+                1,
+                1,
+            );
+            i += 1;
         }
         self.items_box.show_all();
         self.event_source_name
@@ -35,14 +48,19 @@ impl Widget for EventSourceListItem {
 
     view! {
         #[name="items_box"]
-        gtk::Box {
+        gtk::Grid {
             orientation: gtk::Orientation::Vertical,
             margin_start: 10,
             margin_end: 10,
             margin_top: 10,
-            margin_bottom: 10,
+            margin_bottom: 5,
+            row_spacing: 5,
+            column_spacing: 10,
             #[name="event_source_name"]
             gtk::Label {
+                cell: {
+                    width: 2
+                },
                 text: (self.model.event_provider_name.to_string() + " - " + &self.model.config_name).as_str(),
                 xalign: 0.0,
             },
