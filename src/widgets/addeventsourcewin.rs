6524c39039318e89ddae05cffc618b5c58646f88
Emmanuel Touzery <etouzery@gmail.com>
2020-02-16 08:18:55 +0100

    headerbar for the 'add event source' dialog

--- src/widgets/addeventsourcewin.rs
+++ src/widgets/addeventsourcewin.rs
@@ -1,25 +1,94 @@
 use gtk::prelude::*;
-use relm::Widget;
+use relm::{init, Component, Widget};
 use relm_derive::{widget, Msg};
 
 #[derive(Msg)]
+pub enum HeaderMsg {
+    Close,
+    Next,
+}
+
+pub struct HeaderModel {
+    relm: relm::Relm<TitleBar>,
+}
+
+#[widget]
+impl Widget for TitleBar {
+    fn init_view(&mut self) {
+        self.next_btn
+            .get_style_context()
+            .add_class("suggested-action");
+    }
+
+    fn model(relm: &relm::Relm<Self>, _: ()) -> HeaderModel {
+        HeaderModel { relm: relm.clone() }
+    }
+
+    fn update(&mut self, msg: HeaderMsg) {
+        match msg {
+            _ => {}
+        }
+    }
+
+    view! {
+        gtk::HeaderBar {
+            delete_event(_, _) => (HeaderMsg::Close, Inhibit(false)),
+            title: Some("Add event source"),
+            gtk::Button {
+                label: "Close",
+                clicked() => HeaderMsg::Close,
+            },
+            #[name="next_btn"]
+            gtk::Button {
+                label: "Next",
+                child: {
+                    pack_type: gtk::PackType::End
+                },
+                clicked() => HeaderMsg::Next,
+            },
+        }
+    }
+}
+
+#[derive(Msg)]
 pub enum Msg {
-    Quit,
+    Close,
 }
 
 pub struct Model {
     relm: relm::Relm<AddEventSourceWin>,
+    titlebar: Component<TitleBar>,
 }
 
 #[widget]
 impl Widget for AddEventSourceWin {
-    fn model() -> () {}
+    fn init_view(&mut self) {
+        let titlebar = &self.model.titlebar;
+        relm::connect!(
+            titlebar@HeaderMsg::Close,
+            self.model.relm,
+            Msg::Close
+        );
+    }
 
-    fn update(&mut self, _msg: Msg) {}
+    fn model(relm: &relm::Relm<Self>, _: ()) -> Model {
+        Model {
+            relm: relm.clone(),
+            titlebar: init::<TitleBar>(()).expect("Error building the titlebar"),
+        }
+    }
+
+    fn update(&mut self, msg: Msg) {
+        match msg {
+            Msg::Close => self.window.close(),
+        }
+    }
 
     view! {
+        #[name="window"]
         gtk::Window {
-            delete_event(_, _) => (Msg::Quit, Inhibit(false)),
+            delete_event(_, _) => (Msg::Close, Inhibit(false)),
+            titlebar: Some(self.model.titlebar.widget())
         }
     }
 }
