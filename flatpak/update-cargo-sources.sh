a5a4971730fcd0176cd8af4b6b7a0b07ef947743
Emmanuel Touzery <etouzery@gmail.com>
2020-03-25 18:18:22 +0100

    flathub script: update the target folder

--- flatpak/update-cargo-sources.sh
+++ flatpak/update-cargo-sources.sh
@@ -4,4 +4,4 @@ mkdir .cargo
 cargo vendor > .cargo/config
 rm flatpak-cargo-generator.py
 wget https://raw.githubusercontent.com/flatpak/flatpak-builder-tools/d7cfbeaf8d1a2165d917d048511353d6f6e59ab3/cargo/flatpak-cargo-generator.py
-python3 flatpak-cargo-generator.py Cargo.lock -o ../flathub/cargo-sources.json
+python3 flatpak-cargo-generator.py Cargo.lock -o ../com.github.emmanueltouzery.cigale/cargo-sources.json
